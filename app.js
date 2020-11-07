8229c2a5cebf30b659a5d054e07bace1928e78aa
rus tig <svet2000@gmail.com>
2020-11-07 23:39:58 +0500

    client geo and ip, backend ip

--- app.js
+++ app.js
@@ -1,18 +1,68 @@
 const express = require('express');
 const parser = require('ua-parser-js');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// app.get('/', (req, res) => {
+// get Client info to backend
+app.get('/api', async (req, res) => {
+  const ua = await getClientInfo(req);
+  
+  ua.data = await ipLookUp(ua.ip);
+  // write the result as response
+  res.end(JSON.stringify(ua, null, '  '));
+});
 
-// });
+// Google geocode API proxy
+app.get('/geocode', async (req, res) => {
+  const latlng = req.query.latlng;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=${process.env.GOOGLE_GEOCODE_KEY}`;
+  
+  const data = await fetchGeocode(url);
+  console.log(data.results[0]);
+  res.json(data.results[0]);
+});
 
-app.get('/api', (req, res) => {
+app.listen(port, () => console.log(`server is on port: ${port}`));
 
+
+// Lookup via geo ip service
+async function ipLookUp(ip) {
+
+  const url       = `http://ip-api.com/json/${ip}`;             // 45 requests per minute
+  const url_free  = `http://geolocation-db.com/jsonp/${ip}`;    // free and slow (CORS errors)
+  
+  const response = await fetch(url);
+  const data = await response.json()
+  .then(
+    function success(data) {
+      console.log('User\'s Location Data is ', data);
+      console.log('User\'s Country', data.country);
+    },
+
+    function fail(data, status) {
+      console.log('Request failed.  Returned status of', status);
+    }
+  );
+}
+
+// Get reverse geocode data based on lat and lng from google
+async function fetchGeocode(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+// Parse client request data
+function getClientInfo(req) {
   // get user-agent header
-  var ua = parser(req.headers['user-agent']);
+  let ua = parser(req.headers['user-agent']);
+  ua.timestamp = Date.now();
+  // add IP and Proxy
   if (req.headers['via']) { // yes
     ua.ip = req.headers['x-forwarded-for'] || null;
     ua.proxy = req.headers['via'] || null;
@@ -20,11 +70,9 @@ app.get('/api', (req, res) => {
     ua.ip = req.connection.remoteAddress || null;
     ua.proxy = "none";
   }
-  // write the result as response
-  res.end(JSON.stringify(ua, null, '  '));
-})
+  return ua;
+}
 
-app.listen(port, () => console.log(`server is on port: ${port}`));
 
 // REQUESTIP AS MIDDLEWARE
 // const ipMiddleware = function(req, res, next) {
--- main.js
+++ main.js
@@ -1,1 +1,58 @@
-console.log('lconnect');
+/* FRONTEND IP LOCATION */
+geoLocate();
+
+// Use HTML5 Geolocation
+function geoLocate() {
+  if (!navigator.geolocation) {
+    console.log("Geolocation is not supported by your browser");
+    return;
+  }
+
+  function success(position) {
+    // for when getting location is a success
+    const lat  = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log('latitude', lat, 'longitude', lon);
+    reverseGeocode(lat, lon);
+  }
+
+  function error() {
+    // for when getting location results in an error
+    console.error('Cannot retrieve your position. Please enable geolocation in your browser');
+    // get your location some other way
+    return ipLookUp();
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+// Lookup via geo ip service
+async function ipLookUp() {
+
+  const url       = 'http://ip-api.com/json';             // 45 requests per minute
+  const url_free  = 'http://geolocation-db.com/jsonp';    // free and slow (CORS errors)
+  
+  const response = await fetch(url);
+  const data = await response.json()
+  .then(
+    function success(data) {
+      console.log('User\'s Location Data is ', data);
+      console.log('User\'s Country', data.country);
+    },
+
+    function fail(data, status) {
+      console.log('Request failed.  Returned status of', status);
+    }
+  );
+}
+// ipLookUp();
+
+async function reverseGeocode(lat, lon) {
+  const url = `/geocode?latlng=${lat},${lon}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log('url sent: ', url);
+  console.log('data received: ', data);
+}
